af2913fe5ae994018f6d5cd2f8f5053f0425e7e3
ChaewonHan <ChaewonHan@gmail.com>
2020-12-07 20:50:45 +0900

    member

---
+++ 02_JDBC_Member/src/kh/com/member/run/Run.java
@@ -0,0 +1,13 @@
+package kh.com.member.run;
+
+import kh.com.member.controller.MemberController;
+
+public class Run {
+
+	public static void main(String[] args) {
+		MemberController mc = new MemberController();
+		
+		mc.start();
+	}
+
+}
